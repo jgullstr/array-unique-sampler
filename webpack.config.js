bf667c672c0f2923fb73579e847491644234e62c
Josef Gullström <josef@pool.fi>
2018-02-28 19:44:10 +0200

    WP libraryExport

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'build.js',
-        libraryTarget: 'umd'
+        libraryTarget: 'umd',
+        libraryExport: 'default'
     },
     module: {
         rules: [
